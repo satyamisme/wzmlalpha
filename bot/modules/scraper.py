e902c557e441cdb22a492a5cc5ee878ae3a76d31
Satyamisme <satyamisme@gmail.com>
2022-10-31 13:44:53 +0300

    Update scraper.py

--- bot/modules/scraper.py
+++ bot/modules/scraper.py
@@ -130,7 +130,7 @@ def htpmovies(link):
     client = cloudscraper.create_scraper(allow_brotli=False)
     j = url.split('("')[-1]
     url = j.split('")')[0]
-    param = j.replace('/','')[-1]
+    param = url.split("/")[-1]
     DOMAIN = "https://go.kinemaster.cc"
     final_url = f"{DOMAIN}/{param}"
     resp = client.get(final_url)
